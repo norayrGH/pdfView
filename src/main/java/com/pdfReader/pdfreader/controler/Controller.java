37daa31dfae06a5a27b83eefaf98e5b3d0faeb72
Norayr <norayr.gharibyan.java@gmail.com>
2019-10-18 12:57:06 -0700

    EXCLUDE_EMPTY

--- src/main/java/com/pdfReader/pdfreader/controler/Controller.java
+++ src/main/java/com/pdfReader/pdfreader/controler/Controller.java
@@ -4,12 +4,16 @@ import com.pdfReader.pdfreader.PDFInitialization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
 import java.io.*;
 
 @RestController
@@ -23,7 +27,16 @@ public class Controller {
 
         HttpHeaders headers = new HttpHeaders();
 
-        headers.setContentType(MediaType.parseMediaType("application/pdf"));
+
+        Client client = ClientBuilder.newClient();
+        Response responseHEROKU = client.target("https://webtopdf.expeditedaddons.com/?api_key=TBC8540PSL9FGN6Z4K1QJXMIDH3U210653877VAEOWRY92&content=http://www.wikipedia.org&margin=10&html_width=1024&title=My PDF Title")
+                .request(MediaType.TEXT_PLAIN_TYPE)
+                .get();
+
+        System.out.println("status: " + responseHEROKU.getStatus());
+        System.out.println("headers: " + responseHEROKU.getHeaders());
+        System.out.println("body:" + responseHEROKU.readEntity(String.class));
+        headers.setContentType(org.springframework.http.MediaType.parseMediaType("application/pdf"));
         String filename = "AmalyaMuradayanPortfolio.pdf";
         headers.add("content-disposition", "inline;filename=" + filename);
 
