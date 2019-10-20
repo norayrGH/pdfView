bcfccd620f7f45deb0b3326e2b9b316d5dde6173
Norayr <norayr.gharibyan.java@gmail.com>
2019-10-20 05:51:27 -0700

    Final changes test2

--- src/main/java/com/pdfReader/pdfreader/controler/Controller.java
+++ src/main/java/com/pdfReader/pdfreader/controler/Controller.java
@@ -11,9 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.io.*;
 
 @RestController
@@ -25,17 +24,20 @@ public class Controller {
     public ResponseEntity<byte[]> getPDF1() throws IOException {
 
 
-        HttpHeaders headers = new HttpHeaders();
+            HttpHeaders headers = new HttpHeaders();
 
+        try{
+            Client client = ClientBuilder.newClient();
+            Response responseHEROKU = client.target("https://webtopdf.expeditedaddons.com/?api_key=TBC8540PSL9FGN6Z4K1QJXMIDH3U210653877VAEOWRY92&content=http://www.wikipedia.org&margin=10&html_width=1024&title=MyPDFTitle")
+                    .request(MediaType.TEXT_PLAIN_TYPE)
+                    .get();
+            System.out.println("status: " + responseHEROKU.getStatus());
+            System.out.println("headers: " + responseHEROKU.getHeaders());
+            System.out.println("body:" + responseHEROKU.readEntity(String.class));
+        }catch (IncompatibleClassChangeError e ){
+            System.out.println(e.getMessage());
 
-        Client client = ClientBuilder.newClient();
-        Response responseHEROKU = client.target("https://webtopdf.expeditedaddons.com/?api_key=TBC8540PSL9FGN6Z4K1QJXMIDH3U210653877VAEOWRY92&content=http://www.wikipedia.org&margin=10&html_width=1024&title=My PDF Title")
-                .request(MediaType.TEXT_PLAIN_TYPE)
-                .get();
-
-        System.out.println("status: " + responseHEROKU.getStatus());
-        System.out.println("headers: " + responseHEROKU.getHeaders());
-        System.out.println("body:" + responseHEROKU.readEntity(String.class));
+        }
         headers.setContentType(org.springframework.http.MediaType.parseMediaType("application/pdf"));
         String filename = "AmalyaMuradayanPortfolio.pdf";
         headers.add("content-disposition", "inline;filename=" + filename);
